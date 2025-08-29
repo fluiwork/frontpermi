8b801be51f1be6f756c1d82eae31d1821e4940c6
fluiwork <fluiworkagencia@gmail.com>
2025-08-29 15:04:40 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,26 @@
+// app/layout.tsx
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 import './globals.css'
+import { headers } from 'next/headers'
+import ContextProvider from '@/context'
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'AppKit Example App',
+  description: 'Powered by Reown'
+}
+
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const headersObj = await headers()
+  const cookies = headersObj.get('cookie') ?? null
+
   return (
-    <html lang="es">
-      <body>{children}</body>
+    <html lang="en">
+      <body className={inter.className}>
+        <ContextProvider cookies={cookies}>{children}</ContextProvider>
+      </body>
     </html>
   )
 }
