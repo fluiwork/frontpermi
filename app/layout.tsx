7c01f1d2cdf3ddb0e625b89a3f1d8bd78fac3063
fluiwork <fluiworkagencia@gmail.com>
2025-08-29 15:01:25 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,13 @@
-// app/layout.tsx
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
-import { cookies } from 'next/headers' // Cambia de headers a cookies
-import ContextProvider from '@/context'
 
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata: Metadata = {
-  title: 'AppKit Example App',
-  description: 'Powered by Reown'
-}
-
-export default async function RootLayout({ children }: { children: React.ReactNode }) {
-  // Usa cookies() en lugar de headers() para obtener las cookies directamente
-  const cookieStore = await cookies()
-  const cookiesString = cookieStore.toString()
-
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <ContextProvider cookies={cookiesString}>{children}</ContextProvider>
-      </body>
+    <html lang="es">
+      <body>{children}</body>
     </html>
   )
 }
